2abbb3ff1521891f7498d06a029e321fb9859e78
Ilia Pachulia <ilia.pachulia@quantori.com>
2024-07-10 20:29:38 +0400

    replaced search component into header

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -23,12 +23,11 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {Customer} from "./types";
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [ReactiveFormsModule, MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCell, MatCellDef, AsyncPipe, MatHeaderRow, MatRow, MatHeaderRowDef, MatRowDef, MatButton, MatIconModule, MatProgressSpinner, MatFormField, MatInput, MatLabel, FormsModule],
+  imports: [ReactiveFormsModule, MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCell, MatCellDef, AsyncPipe, MatHeaderRow, MatRow, MatHeaderRowDef, MatRowDef, MatButton, MatIconModule, MatProgressSpinner, MatFormField, MatInput, MatLabel],
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css'],
   animations: [trigger('scaleAnimation', [state('small', style({transform: 'scale(1)'})), state('large', style({transform: 'scale(1.1)'})), transition('small <=> large', animate('200ms ease-in'))])]
@@ -38,7 +37,6 @@ export class CustomersComponent implements OnInit {
   displayedColumns: string[] = ['name', 'surname', 'phoneNumber', 'email', 'companyName', 'postActions', 'taskActions'];
   buttonStates: { [id: string]: string } = {};
   isLoading: boolean = false;
-  searchValue: string = '';
 
   constructor(public customersService: CustomersService, private todolistService: TodolistService, private router: Router) {
   }
@@ -46,7 +44,6 @@ export class CustomersComponent implements OnInit {
   ngOnInit(): void {
     this.isLoading = true;
     this.customersService.getCustomers().subscribe(() => this.isLoading = false,)
-
   }
 
   navigateToPosts(customerId: string): void {
@@ -59,10 +56,6 @@ export class CustomersComponent implements OnInit {
 
   onTasksClicked(id: Task['userId']): void {
     this.todolistService.loadToDoListByCustomer(id).subscribe();
-  }
-
-  applyFilter(): void {
-    this.customersService.filterCustomers(this.searchValue);
   }
 
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,37 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {DatePipe} from "@angular/common";
 import {NavigationComponent} from "../navigation/navigation.component";
+import {CustomersService} from "../customers/services/customers.service";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterLink,
-    DatePipe,
-    NavigationComponent
-  ],
+  imports: [RouterLink, DatePipe, NavigationComponent, MatFormField, MatInput, ReactiveFormsModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
   currentDate: Date = new Date();
+  isCustomersPage: boolean = false;
+  searchValue: string = '';
 
-  constructor() {
+  constructor(private customersService: CustomersService, private router: Router) {
   }
 
   ngOnInit(): void {
     setInterval(() => {
       this.currentDate = new Date();
     }, 1000);
+    this.router.events.subscribe(() => {
+      this.isCustomersPage = this.router.url === '/customers';
+    });
+  }
+
+  applyFilter(): void {
+    this.customersService.filterCustomers(this.searchValue);
   }
 }
